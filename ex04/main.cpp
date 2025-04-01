3be12e52c5975ddfdeea8be0d18a363f85d89092
Thibault Dausque <tdausque@2G4.42angouleme.fr>
2025-04-01 13:25:35 +0200

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tdausque <tdausque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:07:53 by thibault          #+#    #+#             */
-/*   Updated: 2025/03/26 15:19:39 by thibault         ###   ########.fr       */
+/*   Updated: 2025/04/01 10:26:04 by tdausque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,40 @@
 #include <string>
 #include <fstream>
 
-void  replace_text(const std::string search, const std::string replace, const std::string filename)
+void replace_text(const std::string search, const std::string replace, const char *filename)
 {
-  std::string      read;
-  std::ofstream    replace_file("test.replace");
-  std::ifstream    file_origin(filename);
-  
-  while (getline(file_origin, read))
-  {
-    replace_file << read;
-  }
+	std::string		f_read;
+	std::ofstream	replace_file("test.replace");
+	std::ifstream	file_origin(filename);
+	std::ifstream	file_replace("test.replace");
+	size_t				pos;
 
-  replace_file.close();
-  file_origin.close();
+	while (getline(file_origin, f_read))
+	{
+		pos = 0;
+		while ((pos = f_read.find(search)) != std::string::npos)
+		{
+			f_read.erase(pos, search.length());
+			f_read.insert(pos, replace);
+		}
+		replace_file << f_read;
+	}
+	replace_file.close();
+	file_origin.close();
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	std::string	filename;
+	const char	*filename;
 	std::string	search;
 	std::string	replace;
 
-  if (ac != 4)
-    return (1);
+	if (ac != 4)
+		return (1);
 
 	filename = av[1];
 	search = av[2];
 	replace = av[3];
-  replace_text(search, replace, filename);
-  return (0);
+	replace_text(search, replace, filename);
+	return (0);
 }
